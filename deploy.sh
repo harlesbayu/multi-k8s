9c22a780ada4b0b9d56c5c757b4616129b370a13
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2020-10-29 10:30:01 +0700

    fixing travis and bash

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t harlesbayu/multi-client:latest -t harlesbayu/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t harlesbayu/multi-server:latest -t harlesbayu/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t harlesbayu/multi-worker:latest -t harlesbayu/multi-worker:$SHA-f ./worker/Dockerfile ./worker
+docker build -t harlesbayu/multi-worker:latest -t harlesbayu/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push harlesbayu/multi-client:latest
 docker push harlesbayu/multi-server:latest
@@ -11,6 +11,6 @@ docker push harlesbayu/multi-server:$SHA
 docker push harlesbayu/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=harlesbayu/multi-server:$SHA
-kubectl set image deployments/client-deployments client=harlesbayu/client-server:$SHA
-kubectl set image deployments/worker-deployments worker=harlesbayu/worker-server:$SHA
+kubectl set image deployments/server-deployment server=harlesbayu/multi-server:$SHA
+kubectl set image deployments/client-deployment client=harlesbayu/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=harlesbayu/multi-worker:$SHA

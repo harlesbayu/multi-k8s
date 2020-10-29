26ad5da4f730b0aa2d5224d0d3d22e987a427d05
Harles Bayu Anggara <bayuanggaraharles@gmail.com>
2020-10-29 10:22:48 +0700

    fixing travis and bash

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t harlesbayu/multi-client -f ./client/Dockerfile ./client
-docker build -t harlesbayu/multi-server -f ./server/Dockerfile ./server
-docker build -t harlesbayu/multi-worker -f ./worker/Dockerfule ./worker
-docker push harlesbayu/multi-client
-docker push harlesbayu/multi-server
-docker push harlesbayu/multi-worker
+docker build -t harlesbayu/multi-client:latest -t harlesbayu/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t harlesbayu/multi-server:latest -t harlesbayu/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t harlesbayu/multi-worker:latest -t harlesbayu/multi-worker:$SHA-f ./worker/Dockerfile ./worker
+
+docker push harlesbayu/multi-client:latest
+docker push harlesbayu/multi-server:latest
+docker push harlesbayu/multi-worker:latest
+
+docker push harlesbayu/multi-client:$SHA
+docker push harlesbayu/multi-server:$SHA
+docker push harlesbayu/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=harlesbayu/multi-server
-kubectl set image deployments/client-deployments server=harlesbayu/client-server
-kubectl set image deployments/worker-deployments server=harlesbayu/worker-server
+kubectl set image deployments/server-deployments server=harlesbayu/multi-server:$SHA
+kubectl set image deployments/client-deployments client=harlesbayu/client-server:$SHA
+kubectl set image deployments/worker-deployments worker=harlesbayu/worker-server:$SHA
